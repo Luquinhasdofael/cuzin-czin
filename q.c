18b79d1192c336be1aff45bd3d1d76f97be29759
Lucas Rafael <lucasrafael10092010@gmail.com>
2026-08-26 10:18:40 -0300

    Add files via upload

---
+++ q.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    double a;
+    scanf("%lf", &a);
+    double b;
+    scanf("%lf", &b);
+    double c;
+    scanf("%lf", &c);
+    double raiz;
+    raiz = pow(b, 2) - (4 * a * c);
+    if (a == 0 || raiz < 0) {
+        printf("Impossivel de calcular\n");
+    }
+    else {
+    double delta = sqrt(raiz);
+    double r1;
+    r1 = (delta + (b * -1)) / (2 * a);
+    double r2;
+    r2 = (delta - (b * -1)) / (2 * a);
+    printf("R1 = %lf\n", r1);
+    printf("R2 = %lf\n", r2);
+    }
+    return 0;
+}
