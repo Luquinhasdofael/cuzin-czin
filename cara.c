c70b985813bc8ee45f15de1820f46a1608203184
Lucas Rafael <lucasrafael10092010@gmail.com>
2026-09-21 09:51:30 -0300

    Add files via upload

---
+++ cara.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+int main(void) {
+    int i; int x; printf("Insira o tamanho da Array: "); scanf("%d", &x);
+    int lista[x];
+    for(i = 0; i < x; i++) {
+        scanf("%d", &lista[x]);
+    }
+    printf("%d\n", lista[0, 1, 2, 3]);
+    return 0;
+}
